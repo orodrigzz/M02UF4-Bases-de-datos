21fd9dcd1eecc6157fc8e9a532a144834a3c21ef
enti <enti@enti.enti>
2022-02-04 11:18:56 +0100

    Servidor Web

--- index.js
+++ index.js
@@ -2,13 +2,36 @@
 
 let http = require("http");
 
+let mongo_client = require("mongodb").MongoClient;
+
+let url = "mongodb://localhost/";
+
+console.log("Inisiando escrí");
+
+mongo_client.connect(url, function(error, conn) {
+	console.log("Dentro");
+
+	if (error){
+		console.log ("ERROR");
+		return; }
+	
+	let db = conn.db("tffhd");
+
+	let info = db.collection("characters").find();
+
+	console.log(info);
+
+});
+
+/*
+
 http.createServer(function(req, res){
 	res.writeHead(200);
 
-	let saludo = "<h1>ola k ase</>";
+	let saludo = "<h1>ola k ase</h1>";
 
 	res.end(saludo);
 
 }).listen(1095);
 
-
+*/
